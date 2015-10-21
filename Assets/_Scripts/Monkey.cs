fd8b209b8b164eecbd49f2b7651b28b25cc9f04d
Sterling Price Springgate <sspringg@umich.edu>
2015-10-21 03:45:39 -0400

    generation works except delete doesn't work yet

--- Assets/_Scripts/Monkey.cs
+++ Assets/_Scripts/Monkey.cs
@@ -16,70 +16,56 @@ public class Monkey : MonoBehaviour {
 	void Start () {
 		S = this;
 		rb = GetComponent<Rigidbody>();
-		rb.AddForce(Vector3.forward * 10);
-//		rb.AddForce(Vector3.up * 10);
+		rb.AddForce(Vector3.forward * 20);
+		rb.AddForce(Vector3.up * 10);
 		mouseXPos = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-//		if(swinging){
-////			Time.timeScale = .1f;
-//			rotationSpeed = 5;	
-//			rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
-//			testPosition = transform.position + rb.velocity * (Time.deltaTime);
-//			if (Vector3.Distance(testPosition,pivot) > tetherLength){   //hand == pivot point t
-//				//pull monkey back to the edge of the his arm
-//				diffVec = (testPosition - pivot);
-//				dist = Math.Sqrt((diffVec.x * diffVec.x) + (diffVec.y * diffVec.y) + (diffVec.z * diffVec.z));
-//				testPosition = pivot + (diffVec / (float)dist) * tetherLength;	//normalize the position
-//				
-//				float speed = rb.velocity.magnitude;
-//				rb.velocity = (testPosition - transform.position) / (Time.deltaTime);
-//				if(speed > rb.velocity.magnitude)
-//					rb.velocity *= (speed / rb.velocity.magnitude);
-//				transform.position = testPosition;
-//			}
-//		}
-//		if(Physics.Raycast(gameObject.transform.position, Vector3.left, out hitInfo, 1f, GetLayerMask(new string[] {"Floor"}))){
-//			print("right");
-//			TileManager.S.currentTile = hitInfo.collider.gameObject;
-//			TileManager.S.curSide = side.right;
-//		}
-//		else if(Physics.Raycast(gameObject.transform.position, Vector3.forward, out hitInfo, 1f, GetLayerMask(new string[] {"Floor"}))){
-//			print("top");
-//			TileManager.S.currentTile = hitInfo.collider.gameObject;
-//			TileManager.S.curSide = side.top;
-//		}
-//		else if(Physics.Raycast(gameObject.transform.position, Vector3.right, out hitInfo, 1f, GetLayerMask(new string[] {"Floor"}))){
-//			print("left");
-//			TileManager.S.currentTile = hitInfo.collider.gameObject;
-//			TileManager.S.curSide = side.left;
-//		}
-//		if (Input.GetMouseButton(0) && swinging == true){		//timescale to 1
-//			print(rb.velocity);
-//			Time.timeScale = 1f;
-//			swinging = false;
-//			mousePos = Input.mousePosition;
-//			mouseXPos = mousePos.x / Screen.width;
-//			mouseXPos = (mouseXPos - .5);
-//			print(rb.velocity);
-//		}
-//		else{	//in air
-//			if(Input.GetKey(KeyCode.A) && rotationSpeed < 10)
-//				rotationSpeed += .5f;
-//			else if(Input.GetKey(KeyCode.S) && rotationSpeed > .5f){
-//				rotationSpeed -= .5f;
-//			}
-//			transform.Rotate(Vector3.up * rotationSpeed);
-//			transform.position = new Vector3(transform.position.x + (float)mouseXPos, (float)transform.position.y, (float)transform.position.z);
-//		}
-		if(Input.GetKey("left"))		//for testing purposes
-			transform.position += Vector3.left;
-		else if(Input.GetKey("right"))
-			transform.position += Vector3.right;
-		transform.position += Vector3.forward;
+		if(swinging){
+//			Time.timeScale = .1f;
+			rotationSpeed = 5;	
+			rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
+			testPosition = transform.position + rb.velocity * (Time.deltaTime);
+			if (Vector3.Distance(testPosition,pivot) > tetherLength){   //hand == pivot point t
+				//pull monkey back to the edge of the his arm
+				diffVec = (testPosition - pivot);
+				dist = Math.Sqrt((diffVec.x * diffVec.x) + (diffVec.y * diffVec.y) + (diffVec.z * diffVec.z));
+				testPosition = pivot + (diffVec / (float)dist) * tetherLength;	//normalize the position
+				
+				float speed = rb.velocity.magnitude;
+				rb.velocity = (testPosition - transform.position) / (Time.deltaTime);
+				if(speed > rb.velocity.magnitude)
+					rb.velocity *= (speed / rb.velocity.magnitude);
+				transform.position = testPosition;
+			}
+		}
+		if (Input.GetMouseButton(0) && swinging == true){		//user releases grab
+			print(rb.velocity);
+			Time.timeScale = 1f;
+			swinging = false;
+			mousePos = Input.mousePosition;
+			mouseXPos = mousePos.x / Screen.width;
+			mouseXPos = (mouseXPos - .5);
+			print(rb.velocity);
+		}
+		else{	//in air
+			if(Input.GetKey(KeyCode.A) && rotationSpeed < 10)
+				rotationSpeed += .5f;
+			else if(Input.GetKey(KeyCode.S) && rotationSpeed > .5f){
+				rotationSpeed -= .5f;
+			}
+			transform.Rotate(Vector3.up * rotationSpeed);
+			transform.position = new Vector3(transform.position.x + (float)mouseXPos, (float)transform.position.y, (float)transform.position.z);
+		}
 	}
+//		if(Input.GetKey("left"))		//for testing purposes
+//			transform.position += Vector3.left;
+//		else if(Input.GetKey("right"))
+//			transform.position += Vector3.right;
+//		transform.position += Vector3.forward;
+//	}
 	public int GetLayerMask(string[] layerNames){
 		int layerMask = 0;
 		foreach(string layer in layerNames){
--- Assets/_Scripts/TileManager.cs
+++ Assets/_Scripts/TileManager.cs
@@ -1,82 +1,165 @@
-﻿//using UnityEngine;
-//using System.Collections;
-//public enum side{
-//	left,
-//	top,
-//	right
-//}
-//public class TileManager : MonoBehaviour {
-//	public static TileManager S;
-//	public GameObject currentTile, leftTile, topTile, rightTile;
-//	public GameObject TreeTrunk;
-//	public GameObject[] TilePrefabs, assignmentPrefabs;
-//	public side curSide = side.top;
-//	
-//	// Use this for initialization
-//	void Start () {
-//		S = this;
-//		Terrain linkedTerrain = gameObject.GetComponent<Terrain>();
-//		
-//		_terrainGrid[0,0] = Terrain.CreateTerrainGameObject(linkedTerrain.terrainData).GetComponent<Terrain>();
-//		_terrainGrid[0,1] = Terrain.CreateTerrainGameObject(linkedTerrain.terrainData).GetComponent<Terrain>();
-//		_terrainGrid[0,2] = Terrain.CreateTerrainGameObject(linkedTerrain.terrainData).GetComponent<Terrain>();
-//		_terrainGrid[1,0] = Terrain.CreateTerrainGameObject(linkedTerrain.terrainData).GetComponent<Terrain>();
-//		_terrainGrid[1,1] = linkedTerrain;
-//		_terrainGrid[1,2] = Terrain.CreateTerrainGameObject(linkedTerrain.terrainData).GetComponent<Terrain>();
-//		_terrainGrid[2,0] = Terrain.CreateTerrainGameObject(linkedTerrain.terrainData).GetComponent<Terrain>();
-//		_terrainGrid[2,1] = Terrain.CreateTerrainGameObject(linkedTerrain.terrainData).GetComponent<Terrain>();
-//		_terrainGrid[2,2] = Terrain.CreateTerrainGameObject(linkedTerrain.terrainData).GetComponent<Terrain>();
-//		
-//		UpdateTerrainPositionsAndNeighbors();
-//	}
-//	
-//	// Update is called once per frame
-//	void Update () {
-//	
-//	}
-//	public void spawnTiles(){
-//		if(curSide == side.left){
-//			//build 2 to the left and 2 more up
-//			currentTile =  (GameObject)Instantiate(TilePrefabs[0], currentTile.transform.GetChild(0).transform.GetChild(0).position, Quaternion.identity);
-//		Instantiate(TreeTrunk, new Vector3(currentTile.transform.position.x , 0, currentTile.transform.position.z ),Quaternion.identity);
-//			//1 up
-//			currentTile =  (GameObject)Instantiate(TilePrefabs[1], currentTile.transform.GetChild(0).transform.GetChild(2).position, Quaternion.identity);
-//		Instantiate(TreeTrunk, new Vector3(currentTile.transform.position.x , 0, currentTile.transform.position.z ),Quaternion.identity);
-//			//1 right
-//			currentTile =  (GameObject)Instantiate(TilePrefabs[2], currentTile.transform.GetChild(0).transform.GetChild(1).position, Quaternion.identity);
-//		Instantiate(TreeTrunk, new Vector3(currentTile.transform.position.x , 0, currentTile.transform.position.z ),Quaternion.identity);
-//			//1 up
-//			currentTile =  (GameObject)Instantiate(TilePrefabs[1], currentTile.transform.GetChild(0).transform.GetChild(2).position, Quaternion.identity);
-//		Instantiate(TreeTrunk, new Vector3(currentTile.transform.position.x , 0, currentTile.transform.position.z ),Quaternion.identity);
-//			//1 left
-//			currentTile =  (GameObject)Instantiate(TilePrefabs[0], currentTile.transform.GetChild(0).transform.GetChild(0).position, Quaternion.identity);
-//		Instantiate(TreeTrunk, new Vector3(currentTile.transform.position.x , 0, currentTile.transform.position.z ),Quaternion.identity);
-//			
-//			}
-//		if(curSide == side.top){
-//			currentTile =  (GameObject)Instantiate(TilePrefabs[1], currentTile.transform.GetChild(0).transform.GetChild(2).position, Quaternion.identity);
-//		Instantiate(TreeTrunk, new Vector3(currentTile.transform.position.x , 0, currentTile.transform.position.z ),Quaternion.identity);
-//			currentTile =  (GameObject)Instantiate(TilePrefabs[1], currentTile.transform.GetChild(0).transform.GetChild(2).position, Quaternion.identity);
-//		Instantiate(TreeTrunk, new Vector3(currentTile.transform.position.x , 0, currentTile.transform.position.z ),Quaternion.identity);
-//			currentTile =  (GameObject)Instantiate(TilePrefabs[1], currentTile.transform.GetChild(0).transform.GetChild(2).position, Quaternion.identity);
-//			
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class InfiniteTerrain : MonoBehaviour
+{
+	public static InfiniteTerrain S;
+	public GameObject Monkey;
+	public GameObject TreeTrunk;
+	private int lastZgenerated, lastYRow = 1;
+	private Terrain[,] map = new Terrain[3,3], newTerrain;
+	
+	void Start ()
+	{
+		S = this;
+		Terrain curTerrain = gameObject.GetComponent<Terrain>();
+		
+		//set all initial map places
+		map[0,0] = Terrain.CreateTerrainGameObject(curTerrain.terrainData).GetComponent<Terrain>();
+		map[0,1] = Terrain.CreateTerrainGameObject(curTerrain.terrainData).GetComponent<Terrain>();
+		map[0,2] = Terrain.CreateTerrainGameObject(curTerrain.terrainData).GetComponent<Terrain>();
+		map[1,0] = Terrain.CreateTerrainGameObject(curTerrain.terrainData).GetComponent<Terrain>();
+		//keep player in middle of map
+		map[1,1] = curTerrain; 
+		//map ahead of player
+		map[1,2] = Terrain.CreateTerrainGameObject(curTerrain.terrainData).GetComponent<Terrain>();
+		map[2,0] = Terrain.CreateTerrainGameObject(curTerrain.terrainData).GetComponent<Terrain>();
+		map[2,1] = Terrain.CreateTerrainGameObject(curTerrain.terrainData).GetComponent<Terrain>();
+		map[2,2] = Terrain.CreateTerrainGameObject(curTerrain.terrainData).GetComponent<Terrain>();
+
+		UpdateTerrain();
+		int initGenerateZ = 100;
+		lastZgenerated = (int)map[0,0].transform.position.z;
+		for(int x = 0; x <= 2; ++x){
+			generateTrees(x, lastYRow, initGenerateZ);
+		}
+		
+	}
+	
+	void UpdateTerrain(){
+		//terrainData.size = total size in world unit of terrain
+		map[0,0].transform.position = new Vector3(
+			map[1,1].transform.position.x - map[1,1].terrainData.size.x,
+			map[1,1].transform.position.y,
+			map[1,1].transform.position.z + map[1,1].terrainData.size.z);
+		map[0,1].transform.position = new Vector3(
+			map[1,1].transform.position.x - map[1,1].terrainData.size.x,
+			map[1,1].transform.position.y,
+			map[1,1].transform.position.z);
+		map[0,2].transform.position = new Vector3(
+			map[1,1].transform.position.x - map[1,1].terrainData.size.x,
+			map[1,1].transform.position.y,
+			map[1,1].transform.position.z - map[1,1].terrainData.size.z);
+		
+		map[1,0].transform.position = new Vector3(
+			map[1,1].transform.position.x,
+			map[1,1].transform.position.y,
+			map[1,1].transform.position.z + map[1,1].terrainData.size.z);
+		map[1,2].transform.position = new Vector3(
+			map[1,1].transform.position.x,
+			map[1,1].transform.position.y,
+			map[1,1].transform.position.z - map[1,1].terrainData.size.z);
+		
+		map[2,0].transform.position = new Vector3(
+			map[1,1].transform.position.x + map[1,1].terrainData.size.x,
+			map[1,1].transform.position.y,
+			map[1,1].transform.position.z + map[1,1].terrainData.size.z);
+		map[2,1].transform.position = new Vector3(
+			map[1,1].transform.position.x + map[1,1].terrainData.size.x,
+			map[1,1].transform.position.y,
+			map[1,1].transform.position.z);
+		map[2,2].transform.position = new Vector3(
+			map[1,1].transform.position.x + map[1,1].terrainData.size.x,
+			map[1,1].transform.position.y,
+			map[1,1].transform.position.z - map[1,1].terrainData.size.z);
+	}
+	
+	void Update(){
+		Vector3 playerPosition = Monkey.transform.position;
+		Terrain curTerrain = null;
+		int curX = 0;
+		int curY = 0;
+		//loop through each element of arrray
+		for (int x = 0; x < 3; x++){
+			for (int y = 0; y < 3; y++){
+				//find the current part of the map our player is in
+				//position is from pivot point(here bottom-left corner)
+				if ((playerPosition.x >= map[x,y].transform.position.x) &&
+					(playerPosition.x <= (map[x,y].transform.position.x + map[x,y].terrainData.size.x))){
+					if((playerPosition.z >= map[x,y].transform.position.z) &&
+					   (playerPosition.z <= (map[x,y].transform.position.z + map[x,y].terrainData.size.z))){
+						curTerrain = map[x,y];	//set which tile the player is in!!
+						curX = 1 - x; //
+						curY = 1 - y;
+						break;
+					}
+				}
+			}
+			if (curTerrain != null)	//found our current Terrain so break
+				break;
+		}
+		//always want player in the middle so fix if she isnt
+			if(curTerrain != map[1,1]){
+				newTerrain = new Terrain[3,3];
+				for (int x = 0; x < 3; x++)
+					for (int y = 0; y < 3; y++){			//(2,0) (1,0), (0,0) [
+						int newX = x + curX;
+						if (newX == 0)
+							newX = 2;
+						else if (newX == 2)
+							newX = 0;
+						int newY = y + curY;
+						if (newY < 0)
+							newY = 2;
+						else if (newY > 2)
+							newY = 0;
+						newTerrain[newX, newY] = map[x,y];
+					}
+			for (int x = 0; x < 3; x++){
+				for (int y = 0; y < 3; y++){
+					map[x, y] = newTerrain[x, y];
+				}
+			}
+				map = newTerrain;
+				UpdateTerrain();
+		}
+		if((lastZgenerated - Monkey.transform.position.z) < 200){
+			
+			for (int k = 0; k < 3; k++)
+				generateTrees(k, lastYRow, 1);	
+			lastZgenerated += Random.Range(45, 59);
+		}
+	}
+	void generateTrees(int x, int y, int zGenDist){
+		float randVal;
+		float tileX = map[x,y].transform.position.x;
+		float tileZ = map[x,y].transform.position.z;
+		float tileSizeX = map[x,y].terrainData.size.x;
+		float tileSizeZ = map[x,y].terrainData.size.z;
+		float i = tileZ, j = tileX;
+		while(i < (lastZgenerated + zGenDist)){
+			while(j < (tileX + tileSizeX)){
+				randVal = Random.value;
+				if(randVal < .5){
+					Vector3 pos = new Vector3(j,0,i);
+					GameObject obj = Instantiate(TreeTrunk, pos, Quaternion.identity) as GameObject;
+					obj.transform.Rotate(-90, 0, 0);
+					obj.transform.parent = map[x,y].transform;
+				}
+				j += Random.Range(45, 59);
+			}
+			j = tileX;
+			i += Random.Range(45, 59);
+		}
+//		List<GameObject> children = new List<GameObject>();
+//		foreach (Transform child in transform){
+//			children.Add(child.gameObject);
 //		}
-//		if(curSide == side.right){
-//			currentTile =  (GameObject)Instantiate(TilePrefabs[2], currentTile.transform.GetChild(0).transform.GetChild(1).position, Quaternion.identity);
-//		Instantiate(TreeTrunk, new Vector3(currentTile.transform.position.x , 0, currentTile.transform.position.z ),Quaternion.identity);
-//			//1 up
-//			currentTile =  (GameObject)Instantiate(TilePrefabs[1], currentTile.transform.GetChild(0).transform.GetChild(2).position, Quaternion.identity);
-//		Instantiate(TreeTrunk, new Vector3(currentTile.transform.position.x , 0, currentTile.transform.position.z ),Quaternion.identity);
-//			//1 left
-//			currentTile =  (GameObject)Instantiate(TilePrefabs[0], currentTile.transform.GetChild(0).transform.GetChild(0).position, Quaternion.identity);
-//		Instantiate(TreeTrunk, new Vector3(currentTile.transform.position.x , 0, currentTile.transform.position.z ),Quaternion.identity);
-//			//1 up
-//			currentTile =  (GameObject)Instantiate(TilePrefabs[1], currentTile.transform.GetChild(0).transform.GetChild(2).position, Quaternion.identity);
-//		Instantiate(TreeTrunk, new Vector3(currentTile.transform.position.x , 0, currentTile.transform.position.z ),Quaternion.identity);
-//			//1 right
-//			currentTile =  (GameObject)Instantiate(TilePrefabs[0], currentTile.transform.GetChild(0).transform.GetChild(0).position, Quaternion.identity);
-//		Instantiate(TreeTrunk, new Vector3(currentTile.transform.position.x , 0, currentTile.transform.position.z ),Quaternion.identity);
-//			
+//		foreach(GameObject childIn in children){
+//			print("destroy");
+//			if((lastZgenerated - childIn.transform.position.z) > 205)
+//				Destroy(childIn);
 //		}
-//	}
-//}
+				
+	}
+}
